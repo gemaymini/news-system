e02ebc933823637e45b14e9f8b1e198dd6ecc92b
gemaymini <446914334@qq.com>
2025-03-30 16:09:29 +0800

    防止db长时间无操作自动断开连接

--- news-api/db/index.js
+++ news-api/db/index.js
@@ -5,4 +5,9 @@ const db = mysql.createConnection({
     password: 'Zrt040522',
     database: 'news_system',
   })
+// 可选：每隔一段时间 ping 一下数据库，保持连接活跃
+setInterval(() => {
+  pool.query('SELECT 1');
+}, 10000); // 每 10 秒 ping 一次
+
   module.exports = db
