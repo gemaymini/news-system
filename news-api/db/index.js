e997369eb83db2d6f8c8140a87377577cb447d20
gemaymini <446914334@qq.com>
2025-03-30 16:35:30 +0800

    修复db bug

--- news-api/db/index.js
+++ news-api/db/index.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
   })
 // 可选：每隔一段时间 ping 一下数据库，保持连接活跃
 setInterval(() => {
-  pool.query('SELECT 1');
+  db.query('SELECT 1');
 }, 10000); // 每 10 秒 ping 一次
 
   module.exports = db
